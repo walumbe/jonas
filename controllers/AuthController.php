dff7330ebc9867bae51756479098f0cc8438cfbd
walumbe <nathanwalumbe.com>
2022-06-16 16:29:46 +0300

    Implemented Logout

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -56,6 +56,12 @@ class AuthController extends Controller
         ]);
     }
 
+    public function logout(Request  $request,Response $response)
+    {
+        Application::$app->logout();
+        $response->redirect('/');
+    }
+
 
 
 }
