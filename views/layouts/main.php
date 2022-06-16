2f9fc7f3d8a72aab12590e0cd1467a05fabbcffd
walumbe <nathanwalumbe.com>
2022-06-16 16:42:02 +0300

    Implemented Profile Page

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -7,27 +7,28 @@ use app\core\Application;
 //echo "</pre>";
 
 ?>
-
 <!doctype html>
 <html lang="en">
-  <head>
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
-    <title>Home</title>
-  </head>
-  <body>
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
+    <link rel="stylesheet" href="/css/bootstrap.css">
+    <title><?php echo $this->title ?></title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
     </button>
+
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
@@ -36,9 +37,9 @@ use app\core\Application;
             <li class="nav-item">
                 <a class="nav-link" href="/contact">Contact</a>
             </li>
-<!--            <li class="nav-item">-->
-<!--                <a class="nav-link" href="/about">About</a>-->
-<!--            </li>-->
+            <!-- <li class="nav-item">
+                <a class="nav-link" href="/about">About</a>
+            </li> -->
         </ul>
         <?php
 
